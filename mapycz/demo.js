4061b7cd399851555ce4b61de4cce30469ee2a19
David Levinsky <david.levinsky@citationtech.net>
2018-11-28 07:52:17 +0100

    fixed mapycz

--- mapycz/demo.js
+++ mapycz/demo.js
@@ -2,5 +2,5 @@
 var browser;
 
 function startDemo() {
-    browser = vts.browser('melown-demo', { 'map': './mapConfig.json' });
+    browser = vts.browser('melown-demo', { 'map': './mapConfig.json', 'positionInUrl': true });
 }
