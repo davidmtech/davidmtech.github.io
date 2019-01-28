ab67f8201e4d1da0d83e6387479e110bdd5b432b
David Levinsky <david.levinsky@citationtech.net>
2019-01-28 14:48:22 +0100

    intergeo demo

---
+++ intergeo2017/params.js
@@ -0,0 +1,108 @@
+
+
+var jsonParams = {
+    "map" : {
+        "map": "https://cdn.melown.com/mario/store/melown2015/map-config/melown/Intergeo-Melown-Earth/mapConfig.json",
+        "position" : ["obj",9.987766,53.552479,"fix",0.00,0.00,-90.00,0.00,231186.89,23.03],
+        "authorization": "https://cdn.melown.com/mario/auth/qduLbzCFlqh8W5PESjzy"
+    },
+    "points" : [
+        {
+            "title" : "Prague (historic city, Bohemia)",
+            "image" : "./thumbs/Prague-Historic-City.jpg",
+            //"position" : ["obj",14.403566,50.087933,"float",0,78.12,-62.32,0.00,707.71,55.00],
+            "position" : ["obj",14.403566,50.087933,"fix",244.2,-78.12,-62.32,0.00,707.71,55.00],
+            "rotate" : 1
+        },
+        {
+            "title" : "Český Krumlov (historic town, Bohemia)",
+            "image" : "./thumbs/Cesky-Krumlov.jpg",
+            "position" : ["obj",14.315856,48.811244,"fix",527.6,-93.05,-33.99,0.00,306.28,55.00],
+            "rotate" : 1
+        },
+        {
+            "title" : "Jerusalem (ancient walled city, Israel/Palestine)",
+            "image" : "./thumbs/Jerusalem.jpg",
+            "position" : ["obj",35.230410,31.777802,"fix",790.3,-267.14,-58.23,0.00,90.00,55.00],
+            "rotate" : 1
+        },
+        {
+            "title" : "Imst (market town, Tyrol)",
+            "image" : "./thumbs/Imst.jpg",
+            "position" : ["obj",10.738664,47.245323,"fix",882.2,-111.06,-50.35,0.00,1782.37,55],
+            "rotate": 0
+        },
+        {
+            "title" : "Treasure Island (neighborhood, San Francisco, California)",
+            "image" : "./thumbs/Treasure-island.jpg",
+            "position" : ["obj",-122.372472,37.824118,"fix",-31,-297.50,-53.96,0.00,910.35,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Znojmo (historic town, Moravia)",
+            "image" : "./thumbs/Znojmo.jpg",
+            "position" : ["obj",16.048152,48.856029,"fix",355.6,-112.21,-60.73,0.00,682.36,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Benešov nad Ploučnicí­ (historic town, Bohemia)",
+            "image" : "./thumbs/Benesov-Nad-Ploucnici.jpg",
+            "position" : ["obj",14.311439,50.741545,"fix",252.6,-318.68,-44.86,0.00,150.00,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Frýdlant (medieval castle, Bohemia)",
+            "image" : "./thumbs/Frydlant.jpg",
+            "position" : ["obj",15.083490,50.914914,"fix",382.8,-56.98,-50.62,0.00,176.20,55],
+            "rotate" : 1
+        },
+        {
+            "title" : "Kokoří­n (medieval castle, Bohemia)",
+            "image" : "./thumbs/Kokorin.jpg",
+            "position" : ["obj",14.576642,50.440340,"fix",348.2,-6.07,-37.68,0.00,150.00,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Křivoklát (medieval castle, Bohemia)",
+            "image" : "./thumbs/Krivoklat.jpg",
+            "position" : ["obj",13.872251,50.037667,"fix",323.39,93.10,-40.22,0.00,220,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Grossglockner (mountain in Tyrol/Carinthia)",
+            "image" : "./thumbs/Grossglockner-mountain.jpg",
+            "position" : ["obj",12.721072,47.086564,"fix",2545.6,-61.66,-32.05,0.00,5367.25,55],
+            "rotate": 2
+        },
+        {
+            "title" : "Mount St Helens (stratovolcano, Washington)",
+            "image" : "./thumbs/Mount-St-Helens.jpg",
+            "position" : ["obj",-122.188130,46.202126,"fix",882.39,-176.11,-15.63,0.00,10766.96,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Pagila Orba (mountain, Corsica)",
+            "image" : "./thumbs/Pagila-Orba.jpg",
+            "position" : ["obj",8.878611,42.342500,"fix",976.2,-193.16,-22.08,0.00,6829.58,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Yangra (mountain, Nepal/Tibet)",
+            "image" : "./thumbs/Yangra.jpg ",
+            "position" : ["obj",85.122039,28.391187,"fix",4569.8,108.39,-39.29,0.00,8232.00,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Karoo (semi-desert plateau, South Africa)",
+            "image" : "./thumbs/Karoo.jpg",
+            "position" : ["obj",19.285509,-33.876371,"fix",711.9,-336.88,-31.75,0.00,68309.37,55],
+            "rotate": 1
+        },
+        {
+            "title" : "Tlustec (former quarry, Bohemia)",
+            "image" : "./thumbs/Tlustec.jpg",
+            "position" : ["obj",14.743600,50.725687,"fix",373.4,-294.60,-30.51,0.00,1396.87,55.00],
+            "rotate": 1
+        }
+    ]
+};
