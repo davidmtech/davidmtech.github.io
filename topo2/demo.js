647dbf38cf4cd18554a6c005749a8608fe62c642
David Levinsky <david.levinsky@citationtech.net>
2019-03-15 10:45:16 +0100

    new demos

---
+++ topo2/demo.js
@@ -0,0 +1,157 @@
+
+var browser;
+
+var selevation = [0,1,4000,1];
+var sefade = 0;
+var senum = 0;
+
+var setable = [
+                [0,1,4000,1],
+                [0,2,4000,1.5],
+                [0,3,4000,2.25]
+        ];
+
+
+function seChange(num, value) {
+    value = parseFloat(value);
+
+    if (selevation[num] != value) {
+        selevation[num] = value;
+        updateSE();
+    }
+}
+
+function fadeChange(value) {
+    value = parseFloat(value);
+
+    if (sefade != value) {
+        sefade = value;
+        updateSE();
+    }
+}
+
+function updateSE() {
+    if (browser && browser.renderer) {
+        var fade = sefade * 0.01;
+        browser.renderer.setSuperElevation(selevation[0], 1 + (selevation[1] - 1) * fade,
+                                           selevation[2], 1 + (selevation[3] - 1) * fade);
+        browser.map.redraw();
+    }
+
+    if (senum != -1) {
+
+        if (selevation[0] != setable[senum][0] || selevation[1] != setable[senum][1] ||
+            selevation[2] != setable[senum][2] || selevation[3] != setable[senum][3]) {
+            senum = -1;
+        } else if (sefade != (senum == 0 ? 0 : 100)) {
+            senum = -1;
+        }
+
+        document.getElementById('se-b1').style.borderStyle = "outset";
+        document.getElementById('se-b2').style.borderStyle = "outset";
+        document.getElementById('se-b3').style.borderStyle = "outset";
+
+        switch(senum) {
+            case 0: document.getElementById('se-b1').style.borderStyle = "inset"; break;
+            case 1: document.getElementById('se-b2').style.borderStyle = "inset"; break;
+            case 2: document.getElementById('se-b3').style.borderStyle = "inset"; break;
+        }
+
+    }
+
+}
+
+function seButton(num) {
+
+    if (num == 0) {
+        sefade = 0;
+    } else {
+        sefade = 100;
+    }
+
+    senum = num;
+    selevation = setable[num].slice();
+
+    var view = browser.map.getView();
+
+    view.options = num == 0 ? {} : {
+        superelevation : [ [selevation[0],selevation[2]], [selevation[1],selevation[3]] ]
+    }
+
+    browser.map.setView(view);
+    
+    document.getElementById('se-h1').value = '' + selevation[0];
+    document.getElementById('se-f1').value = '' + selevation[1];
+    document.getElementById('se-h2').value = '' + selevation[2];
+    document.getElementById('se-f2').value = '' + selevation[3];
+    document.getElementById('se-fade').value = '' + sefade;
+    updateSE();
+}
+
+
+function startDemo() {
+    updateSE();
+
+    var params = vtsParseUrlParams();    
+
+    params['map'] = 'https://cdn.melown.com/vts/melown2015/mapycz/live/mapConfig.json';
+    params['view'] = 'mapycz-default';
+    params['jumpAllowed'] = true;
+    params['positionInUrl'] =  true;
+    params['mapFeaturesReduceMode'] = 'scr-count7';
+
+    if (!params['mapFeaturesReduceParams']) {
+        params['mapFeaturesReduceParams'] =  [0.1,0.35,2];
+    }
+
+    browser = vts.browser('melown-demo', params);
+
+    //hack replace free layers styles
+    browser.on('map-mapconfig-loaded', (function(data){
+
+        var freeLayers = data['view']['freeLayers'];
+
+        if (freeLayers) {
+
+            for (var key in freeLayers) {
+                var layer = freeLayers[key];
+
+                if (layer.style && (typeof layer.style == 'string')) {
+
+                    if (layer.style.indexOf('mapycz.json') != -1) {
+                        layer.style = mapczStyle;
+                    } else if (layer.style.indexOf('ultras.json') != -1) {
+                        layer.style = ultrasStyle;
+                    }
+                }
+            }
+        }
+
+        var namedViews= data['namedViews'];
+
+        for (var key in namedViews) {
+
+            freeLayers = namedViews[key]['freeLayers'];
+
+            if (freeLayers) {
+
+                for (var key2 in freeLayers) {
+                    var layer = freeLayers[key2];
+
+                    if (layer.style && (typeof layer.style == 'string')) {
+
+                        if (layer.style.indexOf('mapycz-aerial.json') != -1) {
+                            layer.style = mapczStyle;
+                        } else if (layer.style.indexOf('ultras-aerial.json') != -1) {
+                            layer.style = ultrasStyle;
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+    }));
+
+}
---
+++ topo2/styles.js
@@ -0,0 +1,151 @@
+var mapczStyle = {
+  "constants": {
+    "@main-font": ["noto-mix","noto-cjk"],
+    "@italic-font": ["noto-italic","noto-mix","noto-cjk"],
+    "@bold-font": ["noto-bold","noto-mix","noto-cjk"],
+    "@icon-circle": ["circle",0,0,63,63],
+    "@icon-hill": ["hill",0,0,63,63],
+    "@base-color": [255,255,255,255],
+    "@base-color2": [0,0,0,255],
+    "@base-stick": [70,5,2,255,255,255,100,0],
+    "@osmid": {"if":[["has","$osm_id"],"$osm_id",""]},
+
+    "@name-solver": {"if":[["has","$name"],{"if":[["any",["!has","$name:en"],["==",{"has-latin":"$name"},true]],"{$name}","{$name}\n{$name:en}"]},""]},
+    "@id-solver": "{@osmid} {@ele-solver} {@name-solver}",
+
+    "@country-label": {"uppercase":{"if":[["==","@name-solver","Schweiz/Suisse/Svizzera/Svizra"],"Switzerland",{"if":[["==","@name-solver","België - Belgique - Belgien"],"België","{@name-solver}"]}]}},
+    "@country-area": 1234,
+
+    "@is-capital": {"if":[["<=",{"str2num":"$capital"},2],1,0]},
+    "@city-label": "{@name-solver}",
+    "@id-solver-town": "@city-label",
+    "@town-population": {"str2num": {"if":[["has","$population"],"$population",1]} },
+
+    "@peak-prominece": {"add":[{"if":[["has","$ele"],{"mul":[0.0001,{"str2num":"$ele"}]},0]},{"if":[["has","$prominence"],{"mul":[0.3048,{"str2num":"$prominence"}]},0]}]},
+
+    "@ele-solver": "{{'round': {'str2num':'$ele'}}} m",
+    "@peak-name": {"if":[["has","$ele"],"{@name-solver}\n{@ele-solver}","{@name-solver}"]},
+  },
+
+  "fonts": {
+    "noto-bold": "//cdn.melown.com/vts/melown2015/fonts/noto-bold/1.0.0/noto-b.fnt",
+    "noto-italic": "//cdn.melown.com/vts/melown2015/fonts/noto-italic/1.0.0/noto-i.fnt",
+    "noto-mix": "//cdn.melown.com/vts/melown2015/fonts/noto-extended/1.0.0/noto.fnt",
+    "noto-cjk": "//cdn.melown.com/vts/melown2015/fonts/noto-cjk/1.0.0/noto.fnt",
+    "#default": "//cdn.melown.com/vts/melown2015/fonts/noto-basic/1.0.0/noto.fnt"
+  },
+  "layers": {
+
+    "country-boundaries": {
+      "filter": ["all",["==","#group","boundary"],["==","$admin_level","2"],["!=","$maritime","1"]],
+      "line": true,
+      "line-flat": false,
+      "line-width": 4,
+      "line-color": [255,255,255,128],
+      "zbuffer-offset": [-0.01,0,0]
+    },
+    "state-boundaries": {
+      "filter": ["all",["==","#group","boundary"],["==","$admin_level","4"],["!=","$maritime","1"]],
+      "line": true,
+      "line-flat": false,
+      "line-width": 4,
+      "line-color": [255,255,255,64],
+      "zbuffer-offset": [-0.01,0,0]
+    },
+    "country-labels": {
+      "visible": false,
+      "filter": ["all",["==","#group","place"],["==","$class","country"]],
+      "importance-source": {"logScale": ["@country-area", 24183000]},
+      "importance-weight": 1,
+      "label": true,
+      "label-font": "@main-font",
+      "label-source": "@country-label",
+      "label-color": "@base-color",
+      "label-color2": "@base-color2",
+      "label-stick": "@base-stick",
+      "label-size": 14,
+      "label-spacing": 1,
+      "label-line-height": 0.8,
+      "label-offset": [0,-8],
+      "label-no-overlap": true,
+      "zbuffer-offset": [-0.15,0,0],
+      "hysteresis": [1500,1500,"@id-solver-town",true]
+    },
+    "towns-labels": {
+      "filter": ["all",["==","#group","place"],["in","$class","city","town","village","hamlet"]],
+      "&imp": {"logScale": ["@town-population", 24183000]},
+      "&rank": {"discrete2":["&imp",[[1,19.71],[19.72,4],[56.04,4],[56.06,3],[63.64,3],[63.66,2],[72.84,2],[72.86,1],[80.47,1],[80.49,0]]]},
+      "importance-source": "&imp",
+      "importance-weight": 1,
+      "label": true,
+      "label-font": "@main-font",
+      "label-source": "@city-label",
+      "label-color": {"linear2":["&rank",[[1,[255,233,0,255]],[5,[230,230,230,255]]]]},
+      "label-stick": {"linear2":["&rank",[[1,[70,5,2,255,233,0,128]],[5,[70,5,2,230,230,230,128]]]]},
+      "label-size": {"discrete2":["&rank",[[0,19],[1,18],[2,17],[3,16],[4,15],[5,14]]]},
+      "label-offset": [0,0],
+      "label-no-overlap": true,
+      "zbuffer-offset": [-0.15,0,0],
+      "hysteresis": [1500,1500,"@id-solver-town",true]
+    },
+
+    "peaks": {
+      "visible": false,
+      "filter": ["all",["==","#group","mountain_peak"],["has","$name"],["!=","$name",""]],
+      "&imp": {"logScale": ["@peak-prominece", 8848.8848]},
+      "&rank": {"discrete2":["&imp",[[1,19.71],[19.72,4],[56.04,4],[56.06,3],[63.64,3],[63.66,2],[72.84,2],[72.86,1],[80.47,1],[80.49,0]]]},
+      "importance-source": "&imp",
+      "importance-weight": 1,
+      "label": true,
+      "label-offset": [0,0],
+      "label-color": {"linear2":["&rank",[[1,[255,233,0,255]],[5,[230,230,230,255]]]]},
+      "label-stick": {"linear2":["&rank",[[1,[70,5,2,255,233,0,128]],[5,[70,5,2,230,230,230,128]]]]},
+      "label-size": {"discrete2":["&rank",[[0,19],[1,18],[2,17],[3,16],[4,15],[5,14]]]},
+      "label-source": "@peak-name",
+      "label-font": "@italic-font",
+      "label-no-overlap": true,
+      "zbuffer-offset": [-0.25,0,0],
+      "culling": 89,
+      "hysteresis": [1500,1500,"@id-solver",true]
+    }
+  }
+};
+
+var ultrasStyle = {
+  "constants": {
+    "@italic-font": ["noto-italic","noto-mix","noto-cjk"],
+    "@name-solver": {"uppercase":{"if":[["has","$name"],"$name","$Name"]}},
+    "@ele": {"if":[["has","$elevation"],"$elevation","$Elevation"]},
+    "@feet": {"round":{"mul":[3.2808399,{"str2num":"@ele"}]}},
+    "@ele-solver": "{{'round': {'str2num':'@ele'}}} m",
+    "@id-solver": "{@ele-solver} {@name-solver}",
+    "@peak-prominece": {"str2num": {"if":[["has","$prom"],"$prom","$Prom"]}},
+  },
+
+  "fonts": {
+    "noto-italic": "//cdn.melown.com/vts/melown2015/fonts/noto-italic/1.0.0/noto-i.fnt",
+    "noto-mix": "//cdn.melown.com/vts/melown2015/fonts/noto-extended/1.0.0/noto.fnt",
+    "noto-cjk": "//cdn.melown.com/vts/melown2015/fonts/noto-cjk/1.0.0/noto.fnt",
+    "#default": "//cdn.melown.com/vts/melown2015/fonts/noto-basic/1.0.0/noto.fnt"
+  },
+
+  "layers": {
+    "peak-labels": {
+      "importance-source": {"logScale": ["@peak-prominece", 8848.8848]},
+      "importance-weight": 1,
+      "visible": true,
+      "label": true,
+      "label-font": "@italic-font",
+      "label-stick": [70,5,2,255,233,0,128],
+      "label-color": [255,233,0,255],
+      "label-size": 19,
+      "label-source": "{@name-solver}\n{@ele-solver}",
+      "label-offset": [0,0],
+      "label-no-overlap": true,
+      "zbuffer-offset": [-1,0,0],
+      "culling": 89,
+      "hysteresis": [1500,1500,"@id-solver",true]
+    }
+  }
+};
+
