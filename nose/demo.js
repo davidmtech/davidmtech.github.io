efcba96aa63acc0240e3bfd1e8e7f61aee167b5d
David Levinsky <david.levinsky@citationtech.net>
2019-04-17 09:35:55 +0200

    nose

---
+++ nose/demo.js
@@ -0,0 +1,22 @@
+
+var browser;
+
+
+function startDemo() {
+
+    var params = vtsParseUrlParams();    
+
+    params['map'] = 'https://rigel.mlwn.se/store/a-3d-mountain-map/map-config/map/mapConfig.json';
+
+    params['jumpAllowed'] = true;
+    params['positionInUrl'] =  true;
+
+    browser = vts.browser('melown-demo', params);
+
+    //hack replace free layers styles
+    browser.on('map-mapconfig-loaded', (function(data){
+
+
+    data['view']['options'] = {};
+
+}
