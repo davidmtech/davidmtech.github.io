8ff1eba532938ddc38ec95430d65504fc0e94059
David Levinsky <david.levinsky@citationtech.net>
2019-04-17 09:48:28 +0200

    nose

--- nose/demo.js
+++ nose/demo.js
@@ -11,6 +11,8 @@ function startDemo() {
     params['jumpAllowed'] = true;
     params['positionInUrl'] =  true;
 
+	params['mapFeaturesReduceParams'] = [ 0.05, 0.17, 11, 0, 1000 ];
+	
     browser = vts.browser('melown-demo', params);
 
     //hack replace free layers styles
